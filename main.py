205f1146740f40ca1ff284828e23c46b810685e3
Nomoreherozz <77875894+Nomoreherozz@users.noreply.github.com>
2023-05-30 10:38:57 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+
+import scheduler
+from task1 import *
+from task2 import *
+from AItask import *
+
+scheduler = scheduler.Scheduler()
+scheduler.SCH_Init()
+# task1 = Task1()
+# task2 = Task2()
+aitask = AItask(0)
+
+
+# scheduler.SCH_Add_Task(task1.Task1Run, 1000, 2000)
+# scheduler.SCH_Add_Task(task2.Task2Run, 2000, 4000)
+scheduler.SCH_Add_Task(aitask.aitaskRun, 3000, 3000)
+
+while True:
+    scheduler.SCH_Update()
+    scheduler.SCH_Dispatch_Tasks()
+    time.sleep(1)
